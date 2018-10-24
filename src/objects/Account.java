19ba3440773b513d484d607ee84a08392e30dfbf
Faruk Becirovic <faruk_becirovic@outlook.com>
2018-10-24 21:24:07 +0200

    version three

--- src/objects/Account.java
+++ src/objects/Account.java
@@ -1,5 +1,57 @@
 package objects;
 
+import java.util.ArrayList;
+
 public class Account {
+	
+	public static ArrayList<Account> accountLog = new ArrayList<>();
+	private int accNum;
+	private String userName;
+	private int booksTaken;
+	
+	Account(int accNum, String userName){
+		this.accNum=accNum;
+		this.userName=userName;
+	}
+
+	public int getAccNum() {
+		return accNum;
+	}
+
+	public void setAccNum(int accNum) {
+		this.accNum = accNum;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public int getBooksTaken() {
+		return this.booksTaken;
+	}
+	
+	public void setBooksTaken(int booksTaken) {
+		this.booksTaken=booksTaken;
+	}
+	
+	public static Account findUser(int accNum) {
+		for(int i=0; i<accountLog.size(); i++)
+			if(accountLog.get(i).accNum==accNum)
+				return accountLog.get(i);
+		
+		return null;
+	}
+	
+	public static boolean isRealAccount (int accNum) {
+		if(findUser(accNum)!=null)
+			return true;
+		
+		else
+			return false;
+	}
 
 }
